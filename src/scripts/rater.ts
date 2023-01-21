c7e7f6497ed514a97e48db45781f9747229fdfb7
Linus Moreau <linus.moreau@gmail.com>
2023-01-21 15:01:57 -0800

    Add line basics

--- src/scripts/rater.ts
+++ src/scripts/rater.ts
@@ -1,14 +1,21 @@
 
 class Line {
-    start: [bigint, bigint];
-    end: [bigint, bigint];
+    start: [number, number];
+    end: [number, number];
+
+    constructor(start: [number, number], end: [number, number]) {
+        this.start = start;
+        this.end = end;
+    }
+
+    print(): void {
+        console.log(`Line from (${this.start}) to (${this.end})`);
+    }
 }
 
 
-function main() {
-    console.log("Hello World")
+export function rate_box() {
+    let line = new Line([1, 2], [4, 3])
+    line.print();
 }
-
-
-main()
 

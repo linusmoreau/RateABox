5248cf9fb7c42b1a26d4ad74f6eebddf7095684d
Linus Moreau <linus.moreau@gmail.com>
2023-01-21 15:23:45 -0800

    Add group_lines structure

--- src/scripts/rater.ts
+++ src/scripts/rater.ts
@@ -8,14 +8,29 @@ class Line {
         this.end = end;
     }
 
-    print(): void {
+    print() : void {
         console.log(`Line from (${this.start}) to (${this.end})`);
     }
 }
 
 
-export function rate_box(lines: Array<[[number, number], [number, number]]>) {
-    let line = new Line([1, 2], [4, 3])
-    line.print();
+function group_lines(lines: Line[]) : Array<Array<Line>> {
+    let group_size;
+    if (lines.length == 9) {
+        group_size = 3;
+    } else {
+        group_size = 4;
+    }
+    return [];
+}
+
+// rate_box receives the lines defining the edges of a box and returns a rating
+// lines must be
+export function rate_box(lines: Array<[[number, number], [number, number]]>) : number {
+    let _lines: Line[] = [];
+    for (let line of lines) {
+        _lines.push(new Line(line[0], line[1]));
+    }
+    return 0;
 }
 

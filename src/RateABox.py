c8e51e258fe4ac59e442200cc2643825e5d7d137
Linus Moreau <linus.moreau@gmail.com>
2023-01-22 11:20:42 -0800

    Add frontend

---
+++ src/RateABox.py
@@ -0,0 +1,48 @@
+import pygame
+import pygame_widgets
+from typing import *
+
+
+lines = []
+grading_menu = False
+start: Union[List[int], None] = None
+
+
+def mousePressed():
+    global start
+    start = pygame.mouse.get_pos()
+
+
+def mouseReleased():
+    global start
+    pos = pygame.mouse.get_pos()
+    lines.append([start, pos])
+    start = None
+    if len(lines) == 9:
+        grading_menu = True
+    print(lines)
+
+
+if __name__ == "__main__":
+    # info = pygame.display.Info()
+    # screen_width, screen_height = info.current_w, info.current_h
+    window = pygame.display.set_mode((600, 400))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            elif event.type == pygame.MOUSEBUTTONUP:
+                mouseReleased()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                mousePressed()
+
+        window.fill((255, 255, 255))
+
+        if start is not None:
+            pygame.draw.line(window, (0, 0, 0), start, pygame.mouse.get_pos())
+
+        for line in lines:
+            pygame.draw.line(window, (0, 0, 0), line[0], line[1])
+
+        pygame.display.flip()

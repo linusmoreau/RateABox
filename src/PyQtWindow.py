c60d560b1425249dc923baac909ce35085d753fb
katielouie <katie.jt.louie@gmail.com>
2023-01-22 09:15:21 -0800

    Add new GUI

---
+++ src/PyQtWindow.py
@@ -0,0 +1,109 @@
+import sys
+from.QtWidgets import *
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        # set the title of main window
+        self.setWindowTitle('Sidebar layout - www.luochang.ink')
+
+        # set the size of window
+        self.Width = 800
+        self.height = int(0.618 * self.Width)
+        self.resize(self.Width, self.height)
+
+        # add all widgets
+        self.btn_1 = QPushButton('1', self)
+        self.btn_2 = QPushButton('2', self)
+        self.btn_3 = QPushButton('3', self)
+        self.btn_4 = QPushButton('4', self)
+
+        # self.btn_1.clicked.connect(self.set_draw)
+        # self.btn_2.clicked.connect(self.set_erase)
+
+        self.initUI()
+
+        # method for widgets
+
+    def make_button(self, icon):
+        button = QPushButton("CLICK", self)
+        button.setStyleSheet("background-image : url(" + icon + ");")
+        return button
+
+    def initUI(self):
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.btn_1)
+        left_layout.addWidget(self.btn_2)
+        left_layout.addWidget(self.btn_3)
+        left_layout.addWidget(self.btn_4)
+        left_layout.addStretch(5)
+        left_layout.setSpacing(20)
+        left_widget = QWidget()
+        left_widget.setLayout(left_layout)
+
+        self.right_widget = QTabWidget()
+        self.right_widget.tabBar().setObjectName("mainTab")
+
+        self.right_widget.setStyleSheet('''QTabBar::tab{width: 0; \
+            height: 0; margin: 0; padding: 0; border: none;}''')
+
+        main_layout = QHBoxLayout()
+        main_layout.addWidget(left_widget)
+        main_layout.addWidget(self.right_widget)
+        main_layout.setStretch(0, 40)
+        main_layout.setStretch(1, 200)
+        main_widget = QWidget()
+        main_widget.setLayout(main_layout)
+        self.setCentralWidget(main_widget)
+
+    # -----------------
+    # buttons
+
+    # TODO: make method
+    # def set_draw(self):
+    #
+    # TODO: make method
+    # def set_erase(self):
+
+    # -----------------
+    # pages
+
+    def ui1(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 1'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
+    def ui2(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 2'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
+    def ui3(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 3'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
+    def ui4(self):
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(QLabel('page 4'))
+        main_layout.addStretch(5)
+        main = QWidget()
+        main.setLayout(main_layout)
+        return main
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Window()
+    ex.show()
+    sys.exit(app.exec_())
